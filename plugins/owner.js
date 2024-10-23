9c6e4ed6336f0bb8b65819658bd01b721f41c5fb
SILENTLOVER432 <newheroku00@gmail.com>
2024-10-23 19:40:15 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,47 +1,41 @@
-const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "owner",
-    react: "👑",
-    alias: ["king","bot"],
-    desc: "get owner number",
+    desc: "im owner",
+    react: "👩‍💻",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const config = await readEnv();
-let madeMenu = `❁ ════ ❃•◯•❃ ════ ❁
-
-*⇆ ʜɪɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
-
-     *${pushname}*
-
-❁ ════ ❃•◯•❃ ════ ❁
-
-*ʜᴇʀᴇ ɪs ʙᴏᴛ ᴏᴡɴᴇʀ ɪɴғᴏ*
-
-⇩━━━━━━━━❁━━━━━━━━⇩
-┝ *ɴᴀᴍᴇ:* *ᴜsᴍᴀɴ.s*
-┝ *ᴘᴜʙʟɪᴄ ɴᴀᴍᴇ:* *sɪʟᴇɴᴛ ʟᴏᴠᴇʀ*
-┝ *ɴɪᴄᴋ ɴᴀᴍᴇ:* *sᴏɴᴜ*
-┝ *ᴀɢᴇ:* *19*
-┝ *ғᴀᴠᴏʀɪᴛᴇ ᴄᴏʟᴏʀ:* *ʙʟᴀᴄᴋ*
-┝ *ғᴀᴠᴏʀɪᴛᴇ ᴀᴘᴘ:* *ᴡʜᴀᴛsᴀᴘᴘ*
-┝ *ᴄᴏɴᴛᴀᴄᴛ* *wa.me/+923096287432*
-┝ *KING OF WHATSAPP*
-❁ ════ ❃•⇆•❃ ════ ❁
-
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ sɪʟᴇɴᴛ_ʟᴏᴠᴇʀ⁴³²*
-
-╰━❁ ═══ ❃•⇆•❃ ═══ ❁━╯
+let owner = ` 
+> *POWERED BY SILENTLOVER432*
 `
-
-await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
-
+await conn.sendMessage(from, { text: owner ,
+  contextInfo: {
+    mentionedJid: [ '' ],
+    groupMentions: [],
+    forwardingScore: 999,
+    isForwarded: false,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: '120363232588171807@newsletter',
+      newsletterName: "SILENTLOVER432",
+      serverMessageId: 999
+    },
+externalAdReply: { 
+title: 'SILENT-SOBX-MD',
+body: `${pushname}`,
+mediaType: 1,
+sourceUrl: "https://wa.me/+923096287432?text=Hy" ,
+thumbnailUrl: "https://telegra.ph/file/3ef5623a33997cf948438.jpg" ,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}
+}}, { quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
 }
-})
+});

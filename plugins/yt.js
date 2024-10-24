330107b7f6c8804d4fcb62f6521f0d132d3fedfe
SILENTLOVER432 <newheroku00@gmail.com>
2024-10-24 09:01:02 +0500

    Update yt.js

--- plugins/yt.js
+++ plugins/yt.js
@@ -129,8 +129,8 @@ TITLE: ${anu.title}
 ❁❁❁❁❁❁❁❁❁❁❁❁❁❁❁❁❁❁`
 await conn.sendMessage(from, { image: { url: anu.thumbnail }, caption: cap}, { quoted: mek })
 const yt2 = await dl.youtubedl(anu.url)
-if (yt2.audio['128kbps'].fileSizeH.includes('MB') && yt2.audio['128kbps'].fileSizeH.replace(' MB','') >= config.MAX_SIZE) return await conn.sendMessage(from, { text: '*This video too big !!*' }, { quoted: mek });
-var du = await yt2.audio['128kbps'].download()
+if (yt2.formats[0].fileSize.includes('MB') && yt2.formats[0].fileSize.replace(' MB','') >= config.MAX_SIZE) return await conn.sendMessage(from, { text: '*This video too big !!*' }, { quoted: mek });
+var du = await yt2.formats[0].download()
     let senda =  await conn.sendMessage(from, { document: { url : du }, mimetype: 'audio/mpeg', fileName: yt2.title + '.mp3',caption: '> ᴍᴜsɪᴄ ʙʏ sɪʟᴇɴᴛ-sᴏʙx-ᴍᴅ ʙᴏᴛ ✅' }, { quoted: mek })
     await conn.sendMessage(from, { react: { text: '🎼', key: senda.key }})
     

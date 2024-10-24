b1ebdb50c1eda20a556e987baf8bd164b620d5e0
SILENTLOVER432 <newheroku00@gmail.com>
2024-10-24 12:24:42 +0500

    Update yt.js

--- plugins/yt.js
+++ plugins/yt.js
@@ -136,8 +136,8 @@ var du = await yt2.audio['128kbps'].download()
     
 await conn.sendMessage(from, { react: { text: '✅', key: mek.key }})
 
-} catch (e) {
-  reply("ERROR ")
-  l(e)
+}catch(e){
+console.log(e)
+reply(`${e}`)
 }
 })

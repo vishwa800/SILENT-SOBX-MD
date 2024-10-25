cf52f9d61306e25f549e24f083b66edfe87bf4f9
SILENTLOVER432 <newheroku00@gmail.com>
2024-10-25 19:08:37 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -3,12 +3,12 @@ let fs = require('fs')
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 cmd({
-    pattern: "ping",
+    pattern: "ping2",
     react: "🤖",
     alias: ["speed"],
     desc: "Check bot\'s ping",
     category: "main",
-    use: '.ping',
+    use: '.ping2',
     filename: __filename
 },
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {

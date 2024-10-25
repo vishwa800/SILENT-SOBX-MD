433535731638dc7da521c31c9f95ee223298991a
SILENTLOVER432 <newheroku00@gmail.com>
2024-10-25 09:30:25 +0500

    Update ping2.js

--- plugins/ping2.js
+++ plugins/ping2.js
@@ -4,6 +4,15 @@ const os = require("os")
 const {runtime} = require('../lib/functions')
 const config = require('../config')
 
+cmd({
+    pattern: "ping2",
+    react: "♻️",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping2',
+    filename: __filename
+},
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const startTime = Date.now()
@@ -15,4 +24,4 @@ const startTime = Date.now()
         console.log(e)
         reply(`${e}`)
     }
-}) 
+})

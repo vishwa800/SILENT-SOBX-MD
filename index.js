e13c7e589832c3bb4e9b4ec376169cd6774cf35d
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-03 22:03:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -207,7 +207,22 @@ if(isReact) return
 m.react("🦋")
 }
 //==========================public react===============//
+function addReactions(m, isReact, senderNumber, botNumber) {
+  const botReactions = ["🥰", "😳", "🤭", "😘", "🤗", "😹"];
+  const userReactions = [
+    "😊", "😁", "😃", "😄", "😆", "🤣",
+    "🎉", "🎊", "👏", "💕", "😍", "🙌",
+    "😳", "😁", "💖", "🩷", "🥰", "🥹",
+    "🤩", "🤯", "🚀", "🔥", "💥", "👽", "🤖", "💻",
+    "🎁", "📦", "🕰️", "📆"
+  ];
 
+  if (!isReact && senderNumber === botNumber) {
+    m.react(botReactions[Math.floor(Math.random() * botReactions.length)]);
+  } else if (!isReact && senderNumber !== botNumber) {
+    m.react(userReactions[Math.floor(Math.random() * userReactions.length)]);
+  }
+        }
 //============================        
         
 //=================================WORKTYPE=========================================== 

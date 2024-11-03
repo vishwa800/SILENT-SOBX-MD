23eba4e06f73815f3b1411a9f55fabdc3a518e0b
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-03 22:40:03 +0500

    Update autoreact.js

--- plugins/autoreact.js
+++ plugins/autoreact.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const {readEnv} = require('../lib/database')
+const config = require('../config')
+cmd({
+  on: "body"
+},
+async(conn, mek, m,{from, isReact, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
 function addReactions(m, isReact, senderNumber, botNumber) {
   const botReactions = ["🥰", "😳", "🤭", "😘", "🤗", "😹"];
   const userReactions = [
@@ -14,3 +23,9 @@ function addReactions(m, isReact, senderNumber, botNumber) {
     m.react(userReactions[Math.floor(Math.random() * userReactions.length)]);
   }
         }
+
+    } catch (error) {
+        console.error(error)
+        reply("An error occurred while processing the message.")
+    }
+})

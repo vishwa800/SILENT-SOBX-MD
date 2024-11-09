66ef027f4a2c77766f87491bcdba3042480a574a
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-09 22:16:28 +0500

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -1,6 +1,5 @@
-const {readEnv} = require('../lib/database')
+const config = require('../config')
 const {cmd , commands} = require('../command')
-
 cmd({
     pattern: "menu2",
     react: "👾",
@@ -10,7 +9,6 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
 let menu = {
 main: '',
 download: '',

afe4716cde817b5fa62463a0efe6877e7080db3c
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-09 22:03:21 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,6 +1,5 @@
-const {readEnv} = require('../lib/database')
+const config = require('../config')
 const {cmd , commands} = require('../command')
-
 cmd({
     pattern: "alive",
     react: "🌐",
@@ -10,7 +9,6 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)

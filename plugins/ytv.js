0e502bb7fcdd61f641469463f9fca831526166a4
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-10 23:13:36 +0500

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,54 +1,108 @@
-const {cmd , commands} = require('../command')
-const fg = require('api-dylux')
+// YT MP3 DOWNLOAD COMMAND 
+
+const { cmd } = require('../command')
 const yts = require('yt-search')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+
+cmd({
+    pattern: "song",
+    alias: ["yta","play","ytmp3"],
+    desc: "download songs.",
+    category: "download",
+    react: "🎧",
+    filename: __filename
+},
+async(conn, mek, m,{from, reply, q}) => {
+try{
+
+if(!q) return reply('Give me song name or url !')
+    
+const search = await fetchJson(`${apilink}/search/yt?q=${q}`)
+const data = search.result.data[0];
+const url = data.url
+    
+const ytdl = await fetchJson(`${apilink}/download/ytmp3?url=${data.url}`)
+    
+let message = `‎‎
+╭═══🎶❃ SILENT-SOBX-MD MUSIC DOWNLOADER ❃🎶═══⊷
+┃❃╭──────────────
+┃❃│🎵 ‎Title: ${data.title}
+┃❃│ ⏱ Duration: ${data.timestamp}
+┃❃│🌏 Uploaded: ${data.ago}
+┃❃│🧿 Views: ${data.views}
+┃❃│🤵 Author: ${data.author.name}
+┃❃│📎 Url: ${data.url}
+┃❃╰───────────────
+╰═════════════════⊷
+> © ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛʟᴏᴠᴇʀ⁴³² 👩‍💻
+
+> ↺ |◁   II   ▷|   ♡
+`
+  
+await conn.sendMessage(from, { image: { url : data.thumbnail }, caption: message }, { quoted : mek })
+  
+// SEND AUDIO NORMAL TYPE and DOCUMENT TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.result.dl_link }, mimetype: "audio/mpeg" }, { quoted: mek })
+await conn.sendMessage(from, { document: { url: ytdl.result.dl_link }, mimetype: "audio/mpeg", fileName: data.title + ".mp3", caption: `${data.title}`}, { quoted: mek })
+  
+} catch(e){
+console.log(e)
+reply(e)
+}
+})
+
+// YT MP4 DOWNLOAD COMMAND 
+
 cmd({
     pattern: "video",
     alias: ["ytv","ytmp4","darama"],
-    desc: "To download videos.",
-    react: "🎥",
-    category: "download",
+    desc: "downlode videos",
+    category: "downlode",
+    react: "🎬",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please give me a url or title")  
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
-    
-    
+if(!q) return reply("*Please give me a title*")
+let search = await yts(q)
+let link = search.all[0].url
 let desc = `
-*_꩜ SILENT-SOBX-MD VIDEO DOWNLOADER ꩜_*
+*──────────────────*
+_*♻️ SILENT-SOBX-MD VIDEO DOWNLOADER ♻️*_
+*──────────────────*
 
-🎥 *VIDEO DATA FOUND!♻️* 
+ *♻️Title :* ${search.all[0].title}
 
-➥ *♻️ TITLE:* ${data.title} 
-➥ *♻️ DURATION:* ${data.timestamp} 
-➥ *♻️ VIEWS:* ${data.views} 
-➥ *♻️ UPLOADED ON:* ${data.ago} 
-➥ *♻️ LINK:* ${data.url} 
+ *♻️Description :* ${search.all[0].description}
 
-🎬 *ENJOY THE VIDEO BROUGHT TO YOU ♻️* 
+ *♻️Duration :* ${search.all[0].timestamp}
 
-> *UPLOADING BY SILENT-SOBX-MD BOT ♻️* 
+ *♻️Ago :* ${search.all[0].ago}
 
-> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛʟᴏᴠᴇʀ⁴³²*
+ *♻️Views :* ${search.all[0].views}
+
+ *♻️URL :* ${search.all[0].url}
+
+> © ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛʟᴏᴠᴇʀ⁴³² 👩‍💻
+
+> ↺ |◁   II   ▷|   ♡
 `
 
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+await conn.sendMessage(from,{image:{url: search.all[0].thumbnail},caption:desc},{quoted:mek})
 
-//download video
 
-let down = await fg.ytv(url)
-let downloadUrl = down.dl_url
+        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?url=${link}`)
 
-//send video message
-await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*© 𝘘𝘜𝘌𝘌𝘕 𝘈𝘕𝘑𝘜 ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ*"},{quoted:mek})
+await conn.sendMessage(from, {
+  video: {url: data.result.downloadLink},
+mimetype: "video/mp4",
+ fileName: `${data.result.title}.mp4`,caption: `*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛʟᴏᴠᴇʀ ···⁴³²* 👩‍💻`}, { quoted: mek })
 
 }catch(e){
-console.log(e)
-  reply('${e}')
+    console.log(e)
+    reply(`${e}`)
 }
-})
-
+}
+)

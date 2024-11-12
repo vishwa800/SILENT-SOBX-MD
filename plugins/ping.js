42c477b66c31448c8682b91b4611bc6478b8c9d7
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-12 23:04:04 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,5 +1,6 @@
 const config = require('../config')
 let fs = require('fs')
+const os = require("os")
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 cmd({
@@ -24,3 +25,24 @@ l(e)
 }
 })
 
+cmd({
+    pattern: "ping",
+    react: "♻️",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const startTime = Date.now()
+        const message = await conn.sendMessage(from, { text: '*_🪄Pinging..._*' })
+        const endTime = Date.now()
+        const ping = endTime - startTime
+        await conn.sendMessage(from, { text: `*♻️ Speed... : ${ping}ms*`}, { quoted: message })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})

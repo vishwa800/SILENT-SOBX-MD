bead71adbd08546975797cacb76fd54a464155ba
Kingamir <nafaskingamir@gmail.com>
2024-11-21 10:20:04 +0430

    Add files via upload

--- plugins/script.js
+++ plugins/script.js
@@ -22,7 +22,7 @@ let repo =`
 *|* *https://whatsapp.com/channel/0029VaHO5B0G3R3cWkZN970s*
 *╰──────────────●●►*
 
-> *POWERED BY SI NOT*
+> *update command Done*
 `
 await conn.sendMessage(from, { text: repo ,
   contextInfo: {

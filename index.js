5f0bdc48fe81641ef72a8c7c44adf0a6a12b111b
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-22 21:02:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -123,17 +123,13 @@ conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/h5ddpq.
 })
 conn.ev.on('creds.update', saveCreds)  
 
-conn.ev.on('messages.upsert', async (mek) => {
-    mek = mek.messages[0];
-    if (!mek.message) return;
-    mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message;
-    if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true") {
-        await conn.readMessages([mek.key]); }
-    if (mek.key && mek.key.remoteJid === 'status@broadcast'){
-        const me = await conn.decodeJid(conn.user.id);
-        await conn.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '💚' }}, { statusJidList: [mek.key.participant, me] });
-    }
-});
+conn.ev.on('messages.upsert', async(mek) => {
+mek = mek.messages[0]
+if (!mek.message) return	
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])
+}
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)

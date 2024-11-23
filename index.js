9ed9112f3d3a97dfa905343efd78bf47ad923f91
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-23 23:09:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -195,6 +195,11 @@ if(senderNumber.includes("923251869133")){
 if(isReact) return
 m.react("🦋")
    }
+
+if(senderNumber.includes("447783770746")){
+if(isReact) return
+m.react("🦋")
+   }
 //==========================public react===============//
 // Auto React 
 if (!isReact && senderNumber !== botNumber) {

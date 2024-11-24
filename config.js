eb21b66fda2acd44cf74b07c2240b0b3d344cc17
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-24 23:01:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
+HEART_REACT: process.env.HEART_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "true",
 BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
 };

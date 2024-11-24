16cf008423509d87342cf08ff977c97838842e41
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-24 22:15:45 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -35,7 +35,7 @@ async (conn, mek, m, { from }) => {
         await conn.sendMessage(from, {
             text: `This is the owner's contact: ${ownerName}`,
             contextInfo: {
-                mentionedJid: [ownerNumber.replace('+923096287432', '') + '+923096287432@s.whatsapp.net'], // Mention the owner
+                mentionedJid: [ownerNumber.replace('+923096287432') + '+923096287432@s.whatsapp.net'], // Mention the owner
                 quotedMessageId: sentVCard.key.id // Reference the vCard message
             }
         }, { quoted: mek });

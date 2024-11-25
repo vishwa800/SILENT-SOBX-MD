695bacb1702abbeecd05931c5594b251d318355e
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-25 17:52:54 +0500

    Update translator.js

--- plugins/translator.js
+++ plugins/translator.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const googleTTS = require('google-tts-api')
 
 cmd({
     pattern: "trt",

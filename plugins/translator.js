e9807ebab7f5cf3ba045975aa887d9a4b712e5b9
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-25 17:27:20 +0500

    Update translator.js

--- plugins/translator.js
+++ plugins/translator.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-const {cmd , commands} = require('../command');
+const config = require('../config')
+const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "trt",
@@ -39,3 +40,25 @@ async (conn, mek, m, { from, q, reply }) => {
         return reply("⚠️ An error occurred data while translating the your text. Please try again later🤕");
     }
 });
+
+//____________________________TTS___________________________
+cmd({
+    pattern: "tts",
+    desc: "download songs",
+    category: "download",
+    react: "👧",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Need some text.")
+    const url = googleTTS.getAudioUrl(q, {
+  lang: 'hi-IN',
+  slow: false,
+  host: 'https://translate.google.com',
+})
+await conn.sendMessage(from, { audio: { url: url }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek })
+    }catch(a){
+reply(`${a}`)
+}
+})

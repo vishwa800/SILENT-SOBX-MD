13353bdf6d8f0007b1b7d450cd7d02bb8b845486
SILENTLOVER432 <newheroku00@gmail.com>
2024-12-01 09:20:35 +0500

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -34,17 +34,23 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*╭──────────●●►*
-*SILENT-SOBX-MD UPTIME LIST↷*
+let status = `
+*[ •  SILENT-SOBX-MD - UPTIME ‎ • ]*
+*╭┈───────────────•*
+*│  ◦* *_UPTIME:➠_*
+*│  ◦* ${runtime(process.uptime())}
+*│  ◦*
+*│  ◦* *_RAM USAGE:➠_*
+*│  ◦* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*│  ◦*
+*│  ◦* *_HOSTNAME:➠_*
+*│  ◦* ${os.hostname()}
+*│  ◦*
+*╰┈───────────────•*
 
-*_UPTIME:➠_*  ${runtime(process.uptime())}
-
-*_RAM USAGE:➠_* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-
-*_HOSTNAME:➠_* ${os.hostname()}
-
-*_OWNER:➠_* *SILENT LOVER⁴³²*
-*╰──────────●●►*
+*•────────────•⟢*
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ sɪʟᴇɴᴛʟᴏᴠᴇʀ⁴³²
+*•────────────•⟢*
 `
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
 

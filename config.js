800ffaead19c213d7f8a917ee207ae0ba3a2017f
SILENTLOVER432 <newheroku00@gmail.com>
2024-12-05 23:23:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "enter your session",
+    CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
